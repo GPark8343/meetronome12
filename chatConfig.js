4afb2b01181a03f039f71ae2356fc4fddafdd69f
GPark <genius8343@gmail.com>
2022-10-24 21:54:31 +0900

    first commit

---
+++ chatConfig.js
@@ -0,0 +1,5 @@
+// chatConfig.js
+export const chatApiKey = 'tjrf7ngdzv5g'
+export const chatUserId = 'demoTestUser1';
+export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGVtb1Rlc3RVc2VyMSJ9.t_0udzUx7JpmtiCSdwaqitg6FLJ5NHUgVRbZBXFwmiw';
+export const chatUserName = 'demoTestUser1';
